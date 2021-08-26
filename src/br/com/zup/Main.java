3bbabb9138b46703b86bd5af0724b75556fc6d7c
Bárbara Ann Lima <babiplima@gmail.com>
2021-08-26 12:02:41 -0300

    Primeiro commit do dia 2021-08-26 - Aula Estruturas condicionais de decição - Exercício 1

---
+++ src/br/com/zup/Main.java
@@ -0,0 +1,31 @@
+package br.com.zup;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Variáveis que recebem valores
+
+        //Instanciando o Scanner
+        Scanner leitor = new Scanner(System.in);
+
+        //Recebendo os números pelos usuários
+
+        System.out.println("Por favor digite o número 1 ");
+        double numero1 = leitor.nextDouble();
+
+        System.out.println("Por favor digite o número 2 ");
+        double numero2 = leitor.nextDouble();
+
+        if (numero1 > numero2){
+            System.out.println("Número 1 é maior que o número 2");
+        }
+        else {
+            System.out.println("Número 2 é maior que o número 1");
+        }
+
+
+    }
+}
